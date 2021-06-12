e35d318ea5a4583a06861a6e731b1e643bf5d32b
Rohaan Ansari <rohaanansari321@gmail.com>
2021-06-12 17:13:25 +0530

    finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,11 +28,24 @@ const Header = (props) => {
 
 
   const handleAuth = () => {
-    auth.signInWithPopup(provider).then((result) => {
-      setUser(result.user);
-    }).catch((error) => {
-      alert(error.message);
-    })
+    if (!userName) {
+      auth.signInWithPopup(provider).then((result) => {
+        setUser(result.user);
+      }).catch((error) => {
+        alert(error.message);
+      })
+
+    } else if (userName) {
+      auth
+        .signOut()
+        .then((result) => {
+          dispatch(setSignOutState())
+          history.push('/');
+        })
+        .catch((err) => {
+          alert(err.message);
+        })
+    }
   }
 
   const setUser = (user) => {
@@ -81,7 +94,13 @@ const Header = (props) => {
                 <span>SERIES</span>
               </a>
             </NavMenu>
-            <UserImg src={userPhoto} alt={userName} />
+            <SignOut>
+              <UserImg src={userPhoto} alt={userName} />
+              <DropDown>
+                <span onClick={handleAuth} >SIGNOUT</span>
+              </DropDown>
+            </SignOut>
+
           </>
       }
     </Nav>
@@ -195,5 +214,42 @@ const Login = styled.a`
 const UserImg = styled.img`
   height: 100%;
 `;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
+`;
+
 
 export default Header;
